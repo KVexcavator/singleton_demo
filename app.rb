ce88fc20e27b5957f941d645228924bcbd9d650f
KVexcavator <str_token@mail.ru>
2018-11-07 17:33:13 +0600

    update Loger

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
 class Loger
+
+  def initialize
+    @f=File.open 'log.txt', 'a'
+  end
+
+  #class mrthod
   def self.say_something
     puts 'Ha-ha-ha'
   end 
-  def self.log_something what
-    f=File.open 'log.txt', 'a'
-    f.puts what
-    f.close
+
+  #instance method
+  def log_something what    
+    @f.puts what
   end
    
 end
 
 
-Loger.say_something
-Loger.log_something 'Balmaley go!'
+Loger.say_something #class method
+
+loger=Loger.new
+loger.log_something 'Balmaley go!' #instance method
